35c85c1f0d8353a3942eabbc2d3fa07f387c0021
Sam Buters <sambuters@gmail.com>
2019-06-20 00:49:06 -0300

    Adding tree cropping

--- projeto_5/Tree.c
+++ projeto_5/Tree.c
@@ -409,48 +409,32 @@ void showTree(Node *root) {
 	int x0, y0, x1, y1;
 	x0 = y0 = x1 = y1 = 0;
 
-	/*for(x0 = 0;;x0++)
+	for(; x0 < size/85;x0++)//top
 		for(int c = 0; c < 85; c++)
 			if(*(treeMatrix + c * 85 + x0) != -77)
 				goto x1Label;
 
-	x1Label:
-	for(x1 = 54;;x1--)
+	x1Label://bottom
+	for(x1 = (size/85) - 1; x1 >= x0; x1--)
 		for(int c = 0; c < 85; c++)
-			if(*(treeMatrix + c * 85 + x1) != -77)
+			if(*(treeMatrix + x1 * 85 + c) != -77)
 				goto y0Label;
 
-	y0Label:
-	for(y0 = 0;;y0++)
+	y0Label://left
+	for(; y0 < 85; y0++)
 		for(int c = 0; c < (size/85); c++)
-			if(*(treeMatrix + y0 * 85 + c) != -77)
+			if(*(treeMatrix + c * 85 + y0) != -77)
 				goto y1Label;
 
 	y1Label:
-	for(y1 = (size/85);;y1--)
+	for(y1 = 85; y1 >= 0; y1--)
 		for(int c = 0; c < (size/85); c++)
-			if(*(treeMatrix + y1 * 85 + c) != -77)
+			if(*(treeMatrix + c * 85 + y1) != -77)
 				goto endLabel;
 
 	endLabel:
-
-	for(int c = y0; c <= y1; c++) {
-		for(int k = x0; k <= x1; k++) {
-			if(*(treeMatrix + c * 85 + k) == -77)
-				printf(" ");
-			else if(*(treeMatrix + c * 85 + k) == -7)
-				printf("/");
-			else if(*(treeMatrix + c * 85 + k) == -9)
-				printf("\\");
-			else
-				printf("%d", *(treeMatrix + c * 85 + k));
-		}
-
-		printf("\n");
-	}*/
-
-	for(int c = y0; c < size/85; c++) {
-		for(int k = x0; k < 85; k++) {
+	for(int c = x0; c < x1 + 1; c++) {
+		for(int k = y0; k <= y1; k++) {
 			if(*(treeMatrix + c * 85 + k) == -77)
 				printf(" ");
 			else if(*(treeMatrix + c * 85 + k) == -7)
@@ -464,11 +448,46 @@ void showTree(Node *root) {
 		printf("\n");
 	}
 
+	/*for(int c = x0; c < x1 + 1; c++) {
+		for(int k = y0; k <= y1; k++) {
+			if(*(treeMatrix + c * 85 + k) == -77)
+				printf(" ");
+			else if(*(treeMatrix + c * 85 + k) == -7)
+				printf("/");
+			else if(*(treeMatrix + c * 85 + k) == -9)
+				printf("\\");
+			else {
+				if(*(treeMatrix + c * 85 + k) < 10)
+					printf("%d", *(treeMatrix + c * 85 + k));
+
+				else {
+					int tenth = 10;
+					int content = *(treeMatrix + c * 85 + k);
+
+					while((content % tenth) != content)
+						tenth*=10;
+
+					while(tenth > 1) {
+						int value = content % tenth;
+
+						value = value/(tenth/10);
+
+						printf("%d", value);
+						k++;
+						tenth = tenth/10;
+					}
+				}
+			}
+		}
+
+		printf("\n");
+	}*/
+
 	free(treeMatrix);
 }
 
 int main(void) {
-	Node *root = readFromFile("bst7.txt");
+	Node *root = readFromFile("bst1.txt");
 
 	if(root == NULL) {
 		root = (Node*) malloc(sizeof(Node));
